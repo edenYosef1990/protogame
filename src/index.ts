ea97591b7bcdcf1fd21017548047f1059d03d454
EDENYO <EDENYO@D-2-MAOZY-WW-N>
2024-04-18 21:48:54 +0300

    exported function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { setUpCanvases } from "./set-up-canvases";
+
 export function add(a: number, b: number): number {
   return a + b;
 }
+
+export { setUpCanvases };
 
 console.log(add(3, 5)); //output: 8
