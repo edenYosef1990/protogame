8ce8c9742eacd2146643f38feafce2099de58162
EDENYO <EDENYO@D-2-MAOZY-WW-N>
2024-04-29 23:54:54 +0300

    changed tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: true,
+  entry: ["src/**/*.ts"],
+  splitting: true,
+  //   target: "es5",
+  format: ["cjs", "esm"],
+  dts: true,
+  treeshake: true,
+  bundle: false,
   clean: true,
 });
