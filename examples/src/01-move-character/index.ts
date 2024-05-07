5089548dad30e401bdee74c0a37ed277bdb885b8
eden <edenyo@rafael.co.il>
2024-05-07 11:37:37 +0300

    changed first example

--- examples/src/01-move-character/index.ts
+++ examples/src/01-move-character/index.ts
@@ -3,6 +3,7 @@ import { DependenciesList } from "../../../src/dependencies-management/get-depen
 import { GraphicsComponent } from "../../../src/entity-object-proxy";
 import { generateCircle } from "../../../src/basic-shapes";
 import { SystemsModuleBuilder } from "../../../src/scheduler";
+import { createSystem } from "../../../src/world/system/create-system";
 import { createSystemWithQueries } from "../../../src/world/system/create-system-with-queries";
 
 export function genBoidPlayer(
@@ -12,21 +13,32 @@ export function genBoidPlayer(
 ) {
   return depedencies.worldsManager.initEntity(
     {
-      renderComp: graphicsDef,
+      graphics: graphicsDef,
     },
     {
-      renderComp: new GraphicsComponent(generateCircle("blue", { x, y }, 10)),
+      graphics: new GraphicsComponent(generateCircle("blue", { x, y }, 10)),
     }
   );
 }
 
-export const initEntitiesSystem = createSystemWithQueries(
-  {},
-  (depedencies, _query) => {
-    genBoidPlayer(depedencies, 100, 100);
+export const initEntitiesSystem = createSystem((depedencies) => {
+  genBoidPlayer(depedencies, 100, 100);
+});
+
+export const moveEntity = createSystemWithQueries(
+  {
+    entities: {
+      graphics: graphicsDef,
+    },
+  },
+  (_, queries) => {
+    let singleEntity = queries.entities[0];
+    let { x, y } = singleEntity.graphics.getPosition();
+    singleEntity.graphics.setPosition({ x, y: y + 1 });
   }
 );
 
 export const gameModule = new SystemsModuleBuilder()
   .addInitSystem(initEntitiesSystem)
+  .addSystem(moveEntity)
   .build();
